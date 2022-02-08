2bb99c450a50935f85be61154dee29a5a58d6391
Monika Sliciute <sliciute.monika@gmail.com>
2022-02-08 15:34:10 +0000

    Replaced App body with a container, removed themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Color(0xFF009688),
-        accentColor: Color(0xFF7C4DFF),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(
-            fontSize: 24,
-            fontStyle: FontStyle.italic,
-          ),
-        ),
-      ),
+      title: "Container App",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Building layouts with Flutter"),
+          title: Text('Container'),
         ),
-        body: Center(
-          child: Text(
-            "Hello Flutter Layouts",
-          ),
+        body: Container(
+          color: Colors.orange,
         ),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.lightbulb_outline),
-            onPressed: () {
-              print("You rang?");
-            }),
-        persistentFooterButtons: <Widget>[
-          IconButton(onPressed: () {}, icon: Icon(Icons.add_comment)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.add_alarm)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.add_location)),
-        ],
       ),
     );
   }
