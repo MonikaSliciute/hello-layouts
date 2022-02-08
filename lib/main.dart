a79be5db5434d506fbee790871c62e96793273ac
Monika Sliciute <sliciute.monika@gmail.com>
2022-02-08 15:07:23 +0000

    Added a basic app bar and text

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Building layouts with Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "Hello Flutter Layouts",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
